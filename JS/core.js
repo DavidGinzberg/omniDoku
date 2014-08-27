a24803910b6063c1d0559456205e65cd2e344350
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-08-27 14:10:52 -0400

    Implemented Accessors
    Implemented getColumn and getBlock accessors. fixes #14

--- JS/core.js
+++ JS/core.js
@@ -25,6 +25,22 @@ function getRow(puzzle, rowNum){
 	return puzzle[rowNum - 1];
 }
 
-function getColumn(puzzle, colNum){return undefined;}
+function getColumn(puzzle, colNum){
+	var column = [];
+	for(var i = 0; i < puzzle.length; i++){
+		column.push(puzzle[i][colNum - 1]);
+	}
+	return column;
+}
 
-function getBlock(puzzle, blockNum){return undefined;}
+function getBlock(puzzle, blockNum){
+	var baseRow = 3 * Math.floor((blockNum - 1)/ 3);
+	var baseCol = 3 * ((blockNum - 1) % 3);
+	var block = [];
+	for(var i = baseRow; i < baseRow + 3; i++){
+		for(var j = baseCol; j < baseCol + 3; j++){
+			block.push(puzzle[i][j]);
+		}
+	}
+	return block;
+}
