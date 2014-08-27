5a4a36caa504eb07b710a766995fda1ec429bd63
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-08-27 00:47:25 -0400

    Added first tests
    Wrote the first real test. Currently failing because core.js is not in
    the gh-pages branch.

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,33 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "Checker test", function( assert ) {
+	var rules = []; //rules haven't been defined yet
+	var validPuzzle = [	[6, 2, 4, 7, 8, 9, 1, 5, 3],
+						[5, 8, 9, 1, 3, 6, 4, 2, 7],
+						[3, 1, 7, 2, 4, 5, 8, 6, 9],
+						[7, 6, 1, 5, 9, 4, 3, 8, 2],
+						[2, 3, 5, 8, 1, 7, 6, 9, 4],
+						[4, 9, 8, 6, 2, 3, 7, 1, 5],
+						[1, 4, 2, 3, 5, 8, 9, 7, 6],
+						[8, 7, 3, 9, 6, 2, 5, 4, 1],
+						[9, 5, 6, 4, 7, 1, 2, 3, 8]];
+	var badRowPuzzle =[	[3, 2, 4, 7, 8, 9, 1, 5, 3],
+						[5, 8, 9, 1, 3, 6, 4, 2, 7],
+						[6, 1, 7, 2, 4, 5, 8, 6, 9],
+						[7, 6, 1, 5, 9, 4, 3, 8, 2],
+						[2, 3, 5, 8, 1, 7, 6, 9, 4],
+						[4, 9, 8, 6, 2, 3, 7, 1, 5],
+						[1, 4, 2, 3, 5, 8, 9, 7, 6],
+						[8, 7, 3, 9, 6, 2, 5, 4, 1],
+						[9, 5, 6, 4, 7, 1, 2, 3, 8]];
+	var badColPuzzle =[	[4, 2, 6, 7, 8, 9, 1, 5, 3],
+						[5, 8, 9, 1, 3, 6, 4, 2, 7],
+						[3, 1, 7, 2, 4, 5, 8, 6, 9],
+						[7, 6, 1, 5, 9, 4, 3, 8, 2],
+						[2, 3, 5, 8, 1, 7, 6, 9, 4],
+						[4, 9, 8, 6, 2, 3, 7, 1, 5],
+						[1, 4, 2, 3, 5, 8, 9, 7, 6],
+						[8, 7, 3, 9, 6, 2, 5, 4, 1],
+						[9, 5, 6, 4, 7, 1, 2, 3, 8]];
+	assert.equal(checkSolution(validPuzzle, rules), true, "A valid 9x9 puzzle should produce a 'true' result");
+	assert.equal(checkSolution(badRowPuzzle, rules), false, "A 9x9 puzzle with errors in a row should produce a 'false' result");
+	assert.equal(checkSolution(badColPuzzle, rules), false, "A 9x9 puzzle with errors in a column should produce a 'false' result");
 });
