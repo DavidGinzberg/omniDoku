5839ef7e6cd5cbe8111c5231c7511701a2652d81
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-08-27 13:38:03 -0400

    Declare accessors
    Declared all three accessors and implemented getRow.

--- JS/core.js
+++ JS/core.js
@@ -2,7 +2,7 @@
 function checkSolution(puzzle, rules){
 	
 	if(rules != undefined){
-		console.warn("Rules have not been implemented yet. Any rules passed to this function will be ignored.");
+		console.warn("The rules argument has not been implemented yet.\nAny rules passed to this function will be ignored.");
 	}
 	
 	//TODO: Check that each row has no duplicates
@@ -20,4 +20,11 @@ function checkSolution(puzzle, rules){
 	console.warn("Not all rules have been implemented. Some invalid puzzles may be incorrectly accepted.");
 	return true; //No errors found, assume the solution is correct
 }
-//TODO: Define a representation of a Sudoku puzzle
+
+function getRow(puzzle, rowNum){
+	return puzzle[rowNum - 1];
+}
+
+function getColumn(puzzle, colNum){return undefined;}
+
+function getBlock(puzzle, blockNum){return undefined;}
