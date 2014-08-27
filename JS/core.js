748669675b9aeaf2c373f8f61ce458e98522c74c
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-08-26 23:14:43 -0400

    Defined checkSolution function
    Defined a function called checkSolution to check whether a solved Sudoku
    puzzle is correct. Currently just blindly returns `true` with some
    console wornings.

---
+++ JS/core.js
@@ -0,0 +1,11 @@
+//TODO: Define a function that takes a set of rules and a solved puzzle and applies the rules to the puzzle.
+function checkSolution(puzzle, rules){
+	
+	if(rules != undefined){
+		console.warn("Rules have not been implemented yet. Any rules passed to this function will be ignored.");
+	}
+	
+	console.warn("No rules currently in place. All puzzles will currently return true."):
+	return true; //No errors found, assume the solution is correct
+}
+//TODO: Define a representation of a Sudoku puzzle
